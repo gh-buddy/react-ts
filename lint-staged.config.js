b2b9f01813c919d5c80a7876b001f80064f4a1a9
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-12-29 18:23:31 +0200

    feat: add package lock file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 // note: all tasks below are ran serially instead of concurrently (controlled by the pre-commit git hook)
 // this avoids the issue of linting and formatting the same file at the same time
 const config = {
-  "*.{js,ts,tsx}": ["pnpm lint", "prettier --write"],
+  "*.{js,ts,tsx}": ["npm run lint", "prettier --write"],
   "*.{css,md}": "prettier --write",
   "package.json": "sort-package-json",
 };
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 export default {
   plugins: {
     autoprefixer: {},
-    postcss-preset-mantine: {},
-    postcss-simple-vars: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
       variables: {
         'mantine-breakpoint-xs': '36em',
         'mantine-breakpoint-sm': '48em',
